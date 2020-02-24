dfd6cd76fb1fcc78c405d53744dcd7463adc5464
Wan Sheng <wsheng1988@gmail.com>
2020-02-23 21:13:58 -0800

    initial commit

---
+++ basic_algorithms_i.js
@@ -0,0 +1,102 @@
+// #1 Print 1-255 (Loop & Recursive)
+
+print1To255 = () => {
+  for(let i = 1; i <= 255 ; i++){
+    console.log(i);
+  }
+}
+
+rPrint1To255 = (i = 1) => {
+  if(i<=255){
+    console.log(i);
+    i++;
+    return rPrint1To255(i);
+  }
+}
+
+
+// #2 Print Odd Numbers 1-100 (Loop & Recursive)
+
+printOddUpTo100 = () => {
+  for (let i = 1; i <= 100; i+=2){
+    console.log(i);
+  }
+}
+
+rPrintOddUpTo100 = (i=1) => {
+  if (i <= 100){
+    console.log(i);
+    i += 2;
+    return rPrintOddUpTo100(i);
+  }
+}
+
+
+// #3 Print Sum from 1 to 5000 (Loop & Recursive)
+
+printSumUpTo5000 = () => {
+  let sum = 0;
+  for (let i = 1; i <= 5000; i++){
+    sum += i;
+  }
+  console.log(sum);
+}
+
+rPrintSumUpTo5000 = (i = 1, sum = 0) => {
+  if (i <= 5000){
+    sum += i;
+    i ++
+    return rPrintSumUpTo5000(i, sum);
+  }
+  console.log(sum);
+}
+
+
+// #4 Iterating Through the Array and Print Each Element (Loop & Recursive)
+
+printElement = arr => {
+  for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+  }
+}
+
+rPrintElement = (arr, i = 0) => {
+  if (i < arr.length){
+    console.log(arr[i]);
+    i ++;
+    return rPrintElement(arr, i);
+  }
+}
+
+
+// #5 Find and Print Array Max (Loop & Recursive)
+
+findMax = arr => {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++){
+    if (max < arr[i]){
+      max = arr[i];
+    }
+  }
+  console.log(max);
+}
+
+
+// #6 Find and Print Array Average
+
+findAverage = arr => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  console.log(sum/arr.length);
+}
+
+rFindAverage = (arr, sum = 0, i = 0) => {
+  if (i < arr.length){
+    sum += arr[i];
+    i ++
+    return rFindAverage(arr, sum, i);
+  }
+  console.log(sum/arr.length);
+}
